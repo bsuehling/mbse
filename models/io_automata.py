75adbaad243a00c98eeb33090bbf38a45042241c
Salim <hertelli@in.tum.de>
2023-07-28 15:34:25 +0200

    implement io automata from behavior

---
+++ models/io_automata.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+from models.behavior import OutMessage
+
+class Transition(BaseModel):
+    pre_state: str
+    post_state: str
+    message_in: str
+    messages_out: List[OutMessage]
+    return_value: Optional[str]
+
+class IOautomat(BaseModel):
+    states: List[str]
+    transitions: List[Transition]
