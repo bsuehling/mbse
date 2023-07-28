761ce9fc3ad740a634c41ff78856fc71f46d41a2
Salim <hertelli@in.tum.de>
2023-07-28 17:47:14 +0200

    add log code in main

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
 from transformations import *
+from transformations.generate_io_automata import GenerateIOAutomata
+import os
+import sys
 
 if __name__ == "__main__":
     input_file = "./input/MDD_Model.xml"
+    artefacts = "artefacts"
+    if not os.path.exists(artefacts):
+        os.makedirs(artefacts)
     interaction_table = XMLToTable(input_file).transform()
     projection = ObjectProjection(interaction_table).transform()
     behaviors = BehaviorExtraction(projection).transform()
+    io_automata = GenerateIOAutomata(behaviors).io_automata()
 
-    for obj, behavior in behaviors.items():
-        print(f"{obj}:")
-        for scenario, behavior in behavior.items():
-            print(f"\t{scenario}:")
-            for behavior_block in behavior:
-                print(f"\t\t{behavior_block}")
-        print("-" * 100)
+    with open(os.path.join(artefacts, "behavior.txt"), "w") as file:
+        original_stdout = sys.stdout
+        sys.stdout = file
+        for obj, behavior in behaviors.items():
+            print(f"{obj}:")
+            for scenario, behavior in behavior.items():
+                print(f"\t{scenario}:")
+                for behavior_block in behavior:
+                    print(f"\t\t{behavior_block}")
+            print("-" * 100)
+        sys.stdout = original_stdout
+
+    with open(os.path.join(artefacts, "io_automata.txt"), "w") as file:
+        original_stdout = sys.stdout
+        sys.stdout = file
+        for obj, automat in io_automata.items():
+            print(f"{obj}:")
+            print(f"states: {automat.states}")
+            for t in automat.transitions:
+                print(f"\tpre_state: {t.pre_state}")
+                print(f"\tpost_state: {t.post_state}")
+                print(f"\tmessage_in: {t.message_in}")
+                print(f"\treturn_value: {t.return_value}")
+                for out in t.messages_out:
+                    print(f"\t\t{out}")
+            print()
+            print("-" * 100)
+        sys.stdout = original_stdout
+
+    from visualization import io_automata_viz
+    
+    # visualize
+    for obj, automat in io_automata.items():
+        io_automata_viz(automat, f"artefacts/{obj}_io_automat.png")
+        
+    
